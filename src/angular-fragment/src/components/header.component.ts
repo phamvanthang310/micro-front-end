3c41c9d2f92315448075acfe70e69f642d1775b6
thangpham <phamvanthang310@gmail.com>
2018-08-16 00:48:55 +0700

    Code update and apply global css Ref: https://toddmotto.com/emulated-native-shadow-dom-angular-2-view-encapsulation#viewencapsulationemulated

--- src/angular-fragment/src/components/header.component.ts
+++ src/angular-fragment/src/components/header.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'angular-header',
   templateUrl: 'header.component.html',
-  encapsulation: ViewEncapsulation.Native
 })
 export class HeaderComponent {
 
